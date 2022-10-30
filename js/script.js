69ba81bb81f3c455a34824d7e1aa8f3f01823d93
Nicolas Ruben Wyplosz <wyplosznicolas@gmail.com>
2022-10-30 17:56:54 -0300

    Validación Formulario

--- js/script.js
+++ js/script.js
@@ -4,3 +4,28 @@ function cambiarContenido(pagina){
     document.getElementById('contenido').src=pagina
     //alert(pagina)
 }
+
+var nombre = document.getElementById('nombre');
+var email = document.getElementById('email');
+var telefono = document.getElementById('telefono');
+
+function validarEnviar(){
+   
+    if(nombre.value === null || nombre.value === ''){
+       alert('Ingrese su nombre completo');
+    }
+
+    if(email.value === null || email.value === ''){
+        alert('Ingrese su correo electrónico');
+    }
+    
+    if(telefono.value === null || telefono.value === ''){
+        alert('Ingrese su número de teléfono');
+    }
+
+    if(nombre.value != null && nombre.value != '' && email.value != null && email.value != '' && telefono.value != null  && telefono.value != ''){
+        alert('Formulario completo. ¡Muchas gracias!')
+    }
+
+   return false;
+    }
