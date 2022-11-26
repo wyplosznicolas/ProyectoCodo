63d43ed1575fc373a05a3c541044703e38d20810
Nicolas Ruben Wyplosz <wyplosznicolas@gmail.com>
2022-11-26 14:03:26 -0300

    Consumiendo API

--- js/script.js
+++ js/script.js
@@ -29,3 +29,36 @@ function validarEnviar(){
 
    return false;
     }
+
+    const api = axios.create({
+        baseURL: 'https://docs.thedogapi.com/'
+      });
+      api.defaults.headers.common['X-API-KEY'] = 'c08d415f-dea7-4a38-bb28-7b2188202e46';
+      
+      const API_URL_RANDOM =  'https://api.thedogapi.com/v1/images/search'
+
+      const spanError = document.getElementById('error')
+  
+  async function loadRandomPerros() {
+    const res = await fetch(API_URL_RANDOM);
+    const data = await res.json();
+    console.log('Random')
+    console.log(data)
+  
+    if (res.status !== 200) {
+      spanError.innerHTML = "Hubo un error: " + res.status;
+    } else {
+      const img1 = document.getElementById('img1');
+      const img2 = document.getElementById('img2');
+      const btn1 = document.getElementById('btn1');
+      const btn2 = document.getElementById('btn2');
+      
+      img1.src = data[0].url;
+      img2.src = data[1].url;
+  
+      btn1.onclick = () => saveFavouriteMichi(data[0].id);
+      btn2.onclick = () => saveFavouriteMichi(data[1].id);
+    }
+  }
+
+  loadRandomPerros();
